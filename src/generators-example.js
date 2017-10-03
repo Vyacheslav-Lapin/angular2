8542e2ca6f1d296ce800890d2f5e71e8e2c3029f
Vyacheslav-Lapin <09vUev1GPSNH4v0e>
2017-10-03 14:42:01 +0300

    Generators for async calls

--- src/generators-example.js
+++ src/generators-example.js
@@ -38,8 +38,8 @@ function* objectEntries(obj) {
     }
 }
 
-let jane = { first: 'Jane', last: 'Doe' };
-for (let [key,value] of objectEntries(jane)) {
+let jane = {first: 'Jane', last: 'Doe'};
+for (let [key, value] of objectEntries(jane)) {
     console.log(`${key}: ${value}`);
 }
 // Output:
@@ -52,6 +52,7 @@ function* foo() {
     yield 'a';
     yield 'b';
 }
+
 function* bar() {
     yield 'x';
     yield* foo();
@@ -71,3 +72,53 @@ function* bla() {
 
 let arr = [...bla()];
 // ['sequence', 'of', 'yielded', 'values']
+
+// Generators for async calls
+it = process();
+it.next();
+
+function asyncAdd(x, y) {
+    setTimeout(() => it.next(x + y), 1000);
+}
+
+function* process() {
+    const res = yield asyncAdd(1, 2);
+    const res2 = yield asyncAdd(res, 3);
+    console.log(res2);
+}
+
+// Generators for async calls - (without generators)
+function asyncAdd2(x, y, f) {
+    setTimeout(() => f(x + y), 1000);
+}
+
+asyncAdd2(1, 2, res =>
+    asyncAdd2(res, 3, res =>
+        console.log(res)));
+
+// Generators for async calls
+function add(x, y) {
+    return new Promise(resolve =>
+        setTimeout(() => resolve(x + y), 1000));
+}
+
+run(function* main() {
+    const res1 = yield add(1, 2);
+    const res2 = yield add(res1, 3);
+    console.log(res2);
+});
+
+function run(g) {
+    const it = g();
+    let ret;
+    const iterate = val => {
+        ret = it.next(val);
+
+        // noinspection JSUnresolvedVariable
+        if (!ret.done)
+            ret.value.then(iterate); // wait on the promise
+        else
+            setTimeout(() => iterate(ret.value), 0);  // avoid synchronous recursion
+    };
+    iterate();
+}
