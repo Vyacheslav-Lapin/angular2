3cc586fadf21a5717652d3a6ef3c4d2cbcc2faf9
Vyacheslav-Lapin <09vUev1GPSNH4v0e>
2017-10-03 10:13:35 +0300

    Initial commit

---
+++ src/Person.js
@@ -0,0 +1,28 @@
+class Person {
+    constructor(name) {
+        // todo validate
+        this._name = name;
+    }
+
+    getInfo() {
+        return `I'm ${this.name}`;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+
+        console.log("проверяем устанавливаемое имя");
+
+        if (name.length <= 3)
+            throw "Имя слишком короткое - должно быть не менее 3-х символов";
+
+        console.log("устанавливаем имя");
+
+        this._name = name;
+
+        console.log("устанавили имя");
+    }
+}
