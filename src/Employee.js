5e320d209a4266c1ffc6afd4e0e70608e8a23658
Vyacheslav-Lapin <09vUev1GPSNH4v0e>
2017-10-03 10:52:57 +0300

    static

--- src/Employee.js
+++ src/Employee.js
@@ -1,8 +1,27 @@
 class Employee extends Person {
+
+    /** @param {Employee} employee */
+    static add(employee) {
+        return this._list.push(employee);
+    }
+
+    static get list() {
+        return this._list.slice();
+    }
+
+    static remove(employee) {
+        const indexOf = this._list.indexOf(employee);
+        if (indexOf > -1)
+            this._list.splice(indexOf, 1);
+        return indexOf;
+    }
+
     constructor(name, salary, position) {
         super(name);
         this.salary = salary;
         this.position = position;
+
+        Employee._list.push(this);
     }
 
     /** @override */
@@ -10,3 +29,6 @@ class Employee extends Person {
         return `${super.getInfo()}, working on ${this.position} position`;
     }
 }
+
+/** @type Array<Employee> */
+Employee._list = [];
